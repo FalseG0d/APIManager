7bf8b1e254a5a22ddd5c196e54bd264373fcb9c0
FalseGodGames <garg.apoorv1098@gmail.com>
2022-11-25 11:21:01 +0530

    Debug=False

--- PortfolioUpdateAPI/settings.py
+++ PortfolioUpdateAPI/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^8ae9xx#x-f_vs)d90uazua9jk)lr0rj-ltj2i-r(2)8_msob5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','portfolioupdateapi.herokuapp.com']
 
